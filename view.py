a6775a1efb0175d217c948d4f4ce1f10eed34605
Erik <erikliu0801@gmail.com>
2019-11-18 11:18:51 +0800

    add html

--- view.py
+++ view.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+   return render_template('hello.html')
+
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
+
+
 from controller import getData
-@app.route("/")
+@app.route("/data")
 def web_getData():
     return getData('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=rdec-key-123-45678-011121314','records')
 
+
+from flask import redirect, url_for, request
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True) # default port:5000
