7d8e255ca670d3b3bfbb9c6cc64574cd9e51021c
Erik <erikliu0801@gmail.com>
2019-11-26 16:38:42 +0800

    new test

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restplus import Resource, Api
 
-# db/orm connect
+## db/orm connect
 # from flask_peewee.db import Database
+# app.config.from_object('config.Configuration')
+# db = Database(app)
+# Set up the cache, a task queue, etc.
 
 app = Flask(__name__)
-# app.config.from_object('config.Configuration')
+api = Api(app)
 
-# db = Database(app)
+todos = {}
 
-# Set up the cache, a task queue, etc.
+@api.route('/<string:todo_id>')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+
+# @api.route('/hello')
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(port=8787)
